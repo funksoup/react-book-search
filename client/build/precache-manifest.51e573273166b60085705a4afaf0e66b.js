6b3943ee05e6db5221131ae143e50c81785634e3
funksoup <josephine@funksoup.com>
2020-08-03 22:50:39 -0400

    initial commit

---
+++ client/build/precache-manifest.51e573273166b60085705a4afaf0e66b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e45c12886d9e600a51d8",
+    "url": "/static/css/main.390e9981.chunk.css"
+  },
+  {
+    "revision": "e45c12886d9e600a51d8",
+    "url": "/static/js/main.65cf55cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "b6f5a444f9668cc785bf67686881b764",
+    "url": "/index.html"
+  }
+];
